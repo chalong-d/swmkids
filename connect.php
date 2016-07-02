8105b6d29ae8faa9db5f2011484697974c10e67f
chalong-d <chalong_d@hotmail.com>
2016-07-02 23:05:21 +0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "cdcc9549";
+$dbname = "swmk";
+$con = mysqli_connect($servername, $username, $password, $dbname);
+if(!$con){die("Couldn't find database");}
+?>
